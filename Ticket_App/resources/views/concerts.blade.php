2936367751c5da27096d074db3daf01f7885a6b1
Tóth Tamás <103049058+C901EE@users.noreply.github.com>
2026-01-21 21:09:08 +0100

    Adding a Nav bar
    Making a layout for it, and routing the basic sites

---
+++ Ticket_App/resources/views/concerts.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-slot:title>
+        Concerts Page
+    </x-slot>
+    <h1 class="text-3xl font-bold underline">
+        Welcome to the Concerts Page!
+    </h1>
+</x-layout>
---
+++ Ticket_App/resources/views/main.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot:title>
+        Home Page
+    </x-slot>
+</x-layout>
